cc18a406fed0ae157ef84def5968fcdf497a3a98
Navpreets602@gmail.com <Navpreets602@gmail.com>
2021-10-06 09:45:21 +0300

    Add project files.

---
+++ CanteenManagement_Navpreet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CanteenManagement_Navpreet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
